2ac33d0e10303931ae309f0ef8ecefc8f320c8be
IsobelSchweigert <66926903+IsobelSchweigert@users.noreply.github.com>
2020-06-18 13:28:19 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+
+
+// This function prints out your name a favorite color
+function nameAndColor(name:string, color:string){
+    console.log("Hello " + name +". Your favorite color is " + color)
+}
+
+nameAndColor("Isobel","Blue")
+
+
+// This function prints out your name and favorite number
+function nameAndNumber(name:string, favNum:number) {
+   console.log( "Hello " + name + ". Your favorite number is " + favNum) 
+}
+
+nameAndNumber( "Isobel ", 4)
+
+
